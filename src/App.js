db9a89252062fada894d63391b90afc8f22e186c
triggering-event <gabriel.j.shapiro@gmail.com>
2019-08-22 21:47:09 -0400

    adds basic home page and login page, including Formik form for Login

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Route, Link} from "react-router-dom";
+import Home from './Components/Home.js';
+import LoginPage from './Components/LoginPage.js';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit derp derp <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Route exact path="/" component={Home}/>
+        <Route path="/loginpage" component={LoginPage}/>
       </header>
     </div>
   );
 }
 
 export default App;
+
