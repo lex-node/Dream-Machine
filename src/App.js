1636ce33c8f802d0afa554c44f8c0fef3fb8c95b
triggering-event <gabriel.j.shapiro@gmail.com>
2019-09-01 20:03:21 -0400

    adds music back to app

--- src/App.js
+++ src/App.js
@@ -5,18 +5,18 @@ import Home from './Components/Home.js';
 import LoginPage from './Components/LoginPage.js';
 import RegistrationPage from './Components/RegistrationPage.js';
 import GraphPage from './Components/GraphPage.js';
-import ContactPage from './Components/ContactPage.js';
+import Music from './Components/Music'
 
 function App() {
 
     return (
         <div className="App">
             <header className="App-header">
+                <Music/>
                 <Route exact path="/" component={Home}/>
                 <Route path="/loginpage" component={LoginPage}/>
                 <Route path="/registrationpage" component={RegistrationPage}/>
                 <Route path="/graphpage" component={GraphPage}/>
-                <Route path="/contactpage" component={ContactPage}/>
             </header>
         </div>
     );
