b1e58531a8abf4bc2849fe3c48472ada4eec6a0e
triggering-event <gabriel.j.shapiro@gmail.com>
2019-09-01 23:27:36 -0400

    Updates

---
+++ precache-manifest.ce1936317925634b90fabb3af908c769.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee3ec66436b33589b01c39952f5e4c78",
+    "url": "/Dream-Machine/index.html"
+  },
+  {
+    "revision": "bccfbe7867b83363e1f9",
+    "url": "/Dream-Machine/static/css/main.c385fa6d.chunk.css"
+  },
+  {
+    "revision": "09fe72078e098e11d0b2",
+    "url": "/Dream-Machine/static/js/2.411f6d92.chunk.js"
+  },
+  {
+    "revision": "bccfbe7867b83363e1f9",
+    "url": "/Dream-Machine/static/js/main.9e7cb2b9.chunk.js"
+  },
+  {
+    "revision": "62b5b0a67149ef6b097c",
+    "url": "/Dream-Machine/static/js/runtime~main.125f209e.js"
+  },
+  {
+    "revision": "73b4112631f7e535eeb3f995c09b80a5",
+    "url": "/Dream-Machine/static/media/(Disc 2) 08 - Darkness Begins (The Soundtrack Album).73b41126.mp3"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Dream-Machine/precache-manifest.ce1936317925634b90fabb3af908c769.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Dream-Machine/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
