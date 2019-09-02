351d0abefcce2a9419ef8c04731ebfa7b550a6b1
triggering-event <gabriel.j.shapiro@gmail.com>
2019-09-01 23:33:18 -0400

    misc. technical fixes after deployment

---
+++ build/precache-manifest.b7149508e37b3b1af51e56dba065f5cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aedcfa904993e0b6f499e92d8b25ecaa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "93c44f7014d82a6f5ec7",
+    "url": "/static/css/main.c385fa6d.chunk.css"
+  },
+  {
+    "revision": "ca82cb146c7fce96851c",
+    "url": "/static/js/2.346ddfda.chunk.js"
+  },
+  {
+    "revision": "93c44f7014d82a6f5ec7",
+    "url": "/static/js/main.5c7e367f.chunk.js"
+  },
+  {
+    "revision": "e9e2578e0d7228bf0b97",
+    "url": "/static/js/runtime~main.f4f54f04.js"
+  },
+  {
+    "revision": "73b4112631f7e535eeb3f995c09b80a5",
+    "url": "/static/media/(Disc 2) 08 - Darkness Begins (The Soundtrack Album).73b41126.mp3"
+  }
+]);
